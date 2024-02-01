833467975fec72e74c18b81847b88f3639b96021
Omar gamal <omar.gamal.m@gmail.com>
2024-02-02 01:52:37 +0200

    Updates

--- components/intro.tsx
+++ components/intro.tsx
@@ -34,8 +34,6 @@ export default function Intro() {
               alt="Ricardo portrait"
               width="192"
               height="192"
-              quality="95"
-              priority={true}
               className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
             />
           </motion.div>
